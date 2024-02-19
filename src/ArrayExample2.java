885135c18c78b032d97e1a8abe2ede4cf92a310d
Tanmay Pal <67100316+tanmay-pal@users.noreply.github.com>
2024-02-19 14:40:48 +0530

    Add files via upload

---
+++ src/ArrayExample2.java
@@ -0,0 +1,11 @@
+class ArrayExample2 {
+
+	public static void main(String args[]) {
+
+		int[] intArray = { 1, 2, 3 };
+
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+	}
+}
